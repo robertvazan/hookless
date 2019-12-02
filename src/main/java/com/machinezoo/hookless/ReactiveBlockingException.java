3cd31d32d209bcaecca68dbb814d4da7236cb82d
Robert Vazan <robvcode@gmail.com>
2019-12-02 09:17:29 +0000

    Added ReactiveBlockingException

---
+++ src/main/java/com/machinezoo/hookless/ReactiveBlockingException.java
@@ -0,0 +1,33 @@
+// Part of Hookless: https://hookless.machinezoo.com
+package com.machinezoo.hookless;
+
+/*
+ * Reactive code should preferably return fallback in case it signals blocking,
+ * because that allows the rest of the code to continue and to trigger further blocking operations.
+ * This way all blocking operations (like database queries) run all in parallel.
+ * 
+ * Sometimes however it is impossible to provide a reasonable fallback result.
+ * In those cases, throwing an exception is acceptable.
+ * Exceptions might be also thrown if fallback value from lower level code cannot be processed by higher level code.
+ * 
+ * This exception class is provided as the most descriptive for cases of reactive blocking.
+ * Its use is however not mandatory. Reactively blocking code may throw any exception.
+ * 
+ * Merely throwing this exception is not sufficient to indicate blocking.
+ * Current reactive computation must be explicitly marked as blocked before throwing.
+ */
+public class ReactiveBlockingException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public ReactiveBlockingException() {
+		super("Reactive computation is blocked.");
+	}
+	public ReactiveBlockingException(Throwable cause) {
+		super("Reactive computation is blocked.", cause);
+	}
+	public ReactiveBlockingException(String message) {
+		super(message);
+	}
+	public ReactiveBlockingException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
