e9b9e4893bde57ff01e95e056d477467d659ea50
Robert Važan <robert.vazan@protonmail.com>
2020-10-26 22:58:36 +0000

    Docs for CurrentReactiveScope

--- src/main/java/com/machinezoo/hookless/CurrentReactiveScope.java
+++ src/main/java/com/machinezoo/hookless/CurrentReactiveScope.java
@@ -11,14 +11,36 @@ import com.machinezoo.stagean.*;
  */
 /**
  * Convenience methods to access current {@link ReactiveScope}.
+ * All calls are forwarded to {@link ReactiveScope#current()} if it is not {@code null}.
+ * If {@link ReactiveScope#current()} is {@code null}, methods of this class provide safe fallback behavior,
+ * which is usually functionally equivalent to creating temporary {@link ReactiveScope}.
+ * 
+ * @see ReactiveScope
+ * @see ReactiveScope#current()
  */
-@StubDocs
+@DraftDocs("reactive blocking link, reactive freezes link, reactive pins link, reactive computation link")
 public class CurrentReactiveScope {
+	/**
+	 * Mark the current reactive computation as reactively blocking.
+	 * If there's no current reactive computation ({@link ReactiveScope#current()} is {@code null}), this method has no effect.
+	 *
+	 * @see ReactiveScope#block()
+	 * @see ReactiveScope#current()
+	 */
 	public static void block() {
 		ReactiveScope current = ReactiveScope.current();
 		if (current != null)
 			current.block();
 	}
+	/**
+	 * Returns {@code true} if the current reactive computation is reactively blocking.
+	 * If there's no current reactive computation ({@link ReactiveScope#current()} is {@code null}), this method returns {@code false}.
+	 * 
+	 * @return {@code true} if the current reactive computation is reactively blocking, {@code false} otherwise
+	 *
+	 * @see ReactiveScope#blocked()
+	 * @see ReactiveScope#current()
+	 */
 	public static boolean blocked() {
 		ReactiveScope current = ReactiveScope.current();
 		if (current != null)
@@ -26,6 +48,22 @@ public class CurrentReactiveScope {
 		else
 			return false;
 	}
+	/**
+	 * Freezes result of specified computation.
+	 * On first call with given {@code key}, returns result of evaluating the {@code supplier}.
+	 * On subsequent calls within the same reactive computation, returns the same result without calling the {@code supplier}.
+	 * If there's no current reactive computation ({@link ReactiveScope#current()} is {@code null}),
+	 * this method evaluates {@code supplier} anew on every call.
+	 * The {@code supplier} should be always the same for given {@code key}.
+	 * 
+	 * @param <T> type of value returned by the {@code supplier}
+	 * @param key identifier for particular frozen value, which implements {@link Object#equals(Object)} and {@link Object#hashCode()}
+	 * @param supplier value supplier to evaluate
+	 * @return value computed by {@code supplier} or previously frozen value
+	 * 
+	 * @see ReactiveScope#freeze(Object, Supplier)
+	 * @see ReactiveScope#current()
+	 */
 	public static <T> T freeze(Object key, Supplier<T> supplier) {
 		ReactiveScope current = ReactiveScope.current();
 		if (current != null)
@@ -33,6 +71,23 @@ public class CurrentReactiveScope {
 		else
 			return supplier.get();
 	}
+	/**
+	 * Pins result of specified computation.
+	 * On first call with given {@code key}, returns result of evaluating the {@code supplier}.
+	 * On subsequent calls within the same sequence of blocking reactive computations (pin lifetime),
+	 * returns the same result without calling the {@code supplier}.
+	 * If there's no current reactive computation ({@link ReactiveScope#current()} is {@code null}),
+	 * this method evaluates {@code supplier} anew on every call.
+	 * The {@code supplier} should be always the same for given {@code key}.
+	 * 
+	 * @param <T> type of value returned by the {@code supplier}
+	 * @param key identifier for particular pinned value, which implements {@link Object#equals(Object)} and {@link Object#hashCode()}
+	 * @param supplier value supplier to evaluate
+	 * @return value computed by {@code supplier} or previously pinned value
+	 * 
+	 * @see ReactiveScope#pin(Object, Supplier)
+	 * @see ReactiveScope#current()
+	 */
 	public static <T> T pin(Object key, Supplier<T> supplier) {
 		ReactiveScope current = ReactiveScope.current();
 		if (current != null)
