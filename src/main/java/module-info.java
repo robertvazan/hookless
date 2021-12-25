fc5f67aab1b16ab13a81000425575a70b8365d78
Robert Važan <robert.vazan@tutanota.com>
2021-12-25 02:33:46 +0000

    Fixes in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,13 @@
 // Part of Hookless: https://hookless.machinezoo.com
+/**
+ * Hookless is a reactive programming library for Java based on invalidate-then-refresh pattern and implemented via thread-local context object.
+ * See the <a href="https://hookless.machinezoo.com/">website</a> for more information.
+ * <p>
+ * The main package {@link com.machinezoo.hookless} contains reactive primitives and other core reactive classes.
+ * Other packages provide reactive wrappers and reactive alternatives to various JRE classes.
+ * 
+ * @see <a href="https://hookless.machinezoo.com/">Hookless website</a>
+ */
 module com.machinezoo.hookless {
 	exports com.machinezoo.hookless;
 	exports com.machinezoo.hookless.util;
@@ -13,5 +22,4 @@ module com.machinezoo.hookless {
 	requires io.opentracing.util;
 	requires it.unimi.dsi.fastutil;
 	requires micrometer.core;
-	requires org.slf4j;
 }
